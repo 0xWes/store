15dd0dfda4f5a1e7899f4408e528fef7ed75b42b
WeslleyJs <weslley2106@gmail.com>
2025-07-18 22:16:46 -0300

    feat: Novos models da aplicação

--- src/main/java/com/example/demo/model/Products.java
+++ src/main/java/com/example/demo/model/Products.java
@@ -4,6 +4,8 @@ package com.example.demo.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.sql.Timestamp;
+
 @Entity
 @Table(name = "products")
 public class Products  extends  Base {
@@ -11,5 +13,37 @@ public class Products  extends  Base {
     private String name;
     private String description;
     private String barcode;
-    private String 
+    private Timestamp validateData;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public Timestamp getValidateData() {
+        return validateData;
+    }
+
+    public void setValidateData(Timestamp validateData) {
+        this.validateData = validateData;
+    }
 }
