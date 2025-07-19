7122a776513fc87cbe24a94c402628019753ef89
WeslleyJs <weslley2106@gmail.com>
2025-07-18 22:15:17 -0300

    first commit

---
+++ src/main/java/com/example/demo/model/Base.java
@@ -0,0 +1,49 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+abstract public class  Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+    private Timestamp created;
+    private Timestamp modified;
+    private Boolean active;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public Timestamp getModified() {
+        return modified;
+    }
+
+    public void setModified(Timestamp modified) {
+        this.modified = modified;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
---
+++ src/main/java/com/example/demo/model/Products.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Products  extends  Base {
+
+    private String name;
+    private String description;
+    private String barcode;
+    private String 
+}
