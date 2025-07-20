18f3695cd6c165cfc0bbf9cec79045af8d9e98db
0xWes <weslley2106@gmail.com>
2025-07-20 20:31:45 -0300

    wip: iniciando criação dos serviços

---
+++ src/main/java/com/example/demo/services/busines/Supplier.java
@@ -0,0 +1,45 @@
+package com.example.demo.services.busines;
+
+import com.example.demo.model.dto.SupplierDTO;
+import com.example.demo.repository.SupplierRepository;
+import com.example.demo.services.ISupplier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class Supplier implements ISupplier {
+
+    private final SupplierRepository supplierRepository;
+
+    public Supplier(SupplierRepository  supplierRepository){
+        this.supplierRepository = supplierRepository;
+    }
+
+    @Override
+    public SupplierDTO registerSupplier(SupplierDTO supplierDTO) {
+        return null;
+    }
+
+    @Override
+    public SupplierDTO getSupplier(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public Page<SupplierDTO> allSuppliers(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public SupplierDTO updateSuplier(SupplierDTO supplierDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteSupplier(UUID uuid) {
+
+    }
+}
